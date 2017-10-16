dfa7aff28820dbe04875963f3bfc4af0cfe9ed5d
yvele <merlicco@gmail.com>
2017-10-16 14:07:36 +0200

    feat: Expose all context properties, except done
    This will make `bindingData` available trough the context object
    
    See https://github.com/christopheranderson/azure-functions-typescript/blob/master/src/context.d.ts
    
    Related to #12 and #6

--- src/IncomingMessage.js
+++ src/IncomingMessage.js
@@ -12,7 +12,7 @@ function removePortFromAddress(address) {
 /**
  * Create a fake connection object
  *
- * @param {Object} context Azure context object for a single HTTP request.
+ * @param {Object} context Raw Azure context object for a single HTTP request
  * @returns {object} Connection object
  */
 function createConnectionObject(context) {
@@ -26,6 +26,29 @@ function createConnectionObject(context) {
 }
 
 /**
+ * Copy usefull context properties from the native context provided by the Azure Function engine
+ *
+ * See:
+ * - https://docs.microsoft.com/en-us/azure/azure-functions/functions-reference-node#context-object
+ * - https://github.com/christopheranderson/azure-functions-typescript/blob/master/src/context.d.ts
+ *
+ * @param {Object} context Raw Azure context object for a single HTTP request
+ * @returns {Object} Filtered context
+ */
+function sanitizeContext(context) {
+  const sanitizedContext = {
+    ...context,
+    log : context.log.bind(context)
+  };
+
+  // We don't want the developper to mess up express flow
+  // See https://github.com/yvele/azure-function-express/pull/12#issuecomment-336733540
+  delete sanitizedContext.done;
+
+  return sanitizedContext;
+}
+
+/**
  * Request object wrapper
  *
  * @private
@@ -33,15 +56,14 @@ function createConnectionObject(context) {
 export default class IncomingMessage extends EventEmitter {
 
   /**
-   * Note: IncomingMessage assumes that all HTTP in is binded to "req" property.
+   * Note: IncomingMessage assumes that all HTTP in is binded to "req" property
    *
-   * @param {Object} context Azure context object for a single HTTP request.
+   * @param {Object} context Sanitized Azure context object for a single HTTP request
    */
   constructor(context) {
     super();
 
-    // Inherit
-    Object.assign(this, context.bindings.req);
+    Object.assign(this, context.bindings.req); // Inherit
 
     this.url = this.originalUrl;
     this.headers = this.headers || {}; // Should always have a headers object
@@ -51,12 +73,7 @@ export default class IncomingMessage extends EventEmitter {
     this.socket = { destroy: NOOP };
     this.connection = createConnectionObject(context);
 
-    // Add access specific objects via req.context
-    this.context = {
-      log           : context.log.bind(context),
-      invocationId  : context.invocationId,
-      bindings      : context.bindings
-    };
+    this.context = sanitizeContext(context); // Specific to Azure Function
   }
 
 }
--- test/IncomingMessage.test.js
+++ test/IncomingMessage.test.js
@@ -51,4 +51,41 @@ describe("IncomingMessage", () => {
     });
   });
 
+  it("Should work with a full native context object", () => {
+
+    const context = {
+      invocationId : "f0f6e586-0b79-4407-aa53-97919f45eba5",
+      bindingData : { foo: "bar" },
+      bindings : {
+        req : {
+          originalUrl : "http://foo.com/bar"
+        }
+      },
+      log   : () => {},
+      done  : () => {}
+    };
+
+    const req = new IncomingMessage(context);
+    req.resume();
+    req.socket.destroy();
+
+    expect(req).toMatchObject({
+      url         : "http://foo.com/bar",
+      connection  : {
+        encrypted     : false,
+        remoteAddress : undefined
+      }
+    });
+
+    expect(req.context).toBeDefined();
+    expect(req.context).not.toBe(context);
+    expect(req.context.invocationId).toBe(context.invocationId);
+    expect(req.context.bindingData).toBe(context.bindingData);
+    expect(req.context.bindings).toBe(context.bindings);
+    expect(req.context.log).not.toBe(context.log);
+    expect(req.context.log).toBeInstanceOf(Function);
+    expect(req.context.done).toBeUndefined(); // We don't want to pass done
+
+  });
+
 });
